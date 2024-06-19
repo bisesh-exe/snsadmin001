ca34e39d726d4de024c1888b939ea49a42c03a07
Bisesh-exe <biseshgautam300@gmail.com>
2024-06-19 22:56:44 +0545

    Update Userpage.jsx

--- src/Userpage.jsx
+++ src/Userpage.jsx
@@ -28,7 +28,7 @@ function Userpage() {
         </div>
       </div>
       <footer className="bg-dark text-white text-center py-3 mt-auto">
-        <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+        <p>&copy; 2024 SNS Accounting. All rights reserved.</p>
       </footer>
     </div>
   );
